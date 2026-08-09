449bc0f6debe4301e357dc5916736f36550bca80
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-08-09 10:54:02 +0700

    feat: CRUD products route api

---
+++ models/Product.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IProduct extends Document {
+    name: string;
+    price: number;
+    description: string;
+}
+
+const ProductSchema = new Schema<IProduct>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default mongoose.models.Product || mongoose.model<IProduct>("Product", ProductSchema);
